de66365d07ea05c00ab490e5d8d9e10ec974b4b0
unitedastronomymedia <unitedastronomymedia@gmail.com>
2026-02-23 21:52:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function enterPortal() {
   document.getElementById("gate").style.display = "none";
   document.querySelector(".container").style.display = "block";
 }
+
 function updateTime() {
   const now = new Date();
   document.getElementById("time").innerText =
@@ -10,3 +11,14 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
+
+const checkbox = document.getElementById("agree");
+const button = document.getElementById("enterBtn");
+
+checkbox.addEventListener("change", function() {
+  if (checkbox.checked) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+});
